92548f8efb4fcf801306148b085327423cbb8859
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-10 17:17:49 +0530

    Create 16*16 grid
    Grid conatiner holds 16*16 square divs added dynamically

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const gridContainer = document.querySelector("#grid-container");
+
+// Grid contains 16*16 square divs
+for (let i = 0; i < 16; i++) {
+    const gridRow = createGridRow(i);
+    gridContainer.appendChild(gridRow);
+
+    for (let j = 0; j < 16; j++) {
+        const squareDiv = createSquareDiv(); 
+        gridRow.appendChild(squareDiv);
+    }
+}
+
+function createGridRow (row) {
+    const gridRow = document.createElement("div");
+    gridRow.setAttribute("id", `row${row}`);
+    gridRow.setAttribute("class", "grid-row");
+    return gridRow;
+}
+
+function createSquareDiv() {
+    const squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class", "square-div");
+    return squareDiv;
+}
