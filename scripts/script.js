cac69b2b58120fd4d7b2cf33a73e5e4b0c7e81d2
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-11 22:42:47 +0530

    Change the grid size on prompt
    Remove the old grid and create a new grid of size given by the user

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,18 @@
 const gridContainer = document.querySelector("#grid-container");
+const gridSizeButton = document.querySelector("#prompt-size");
 
-// Grid contains 16*16 square divs
-for (let i = 0; i < 16; i++) {
-    const gridRow = createGridRow(i);
-    gridContainer.appendChild(gridRow);
+gridContainer.addEventListener("mouseover", addBackgroundColor);
+gridSizeButton.addEventListener("click", changeGrid);
 
-    for (let j = 0; j < 16; j++) {
-        const squareDiv = createSquareDiv(); 
-        gridRow.appendChild(squareDiv);
+function createGrid (gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const gridRow = createGridRow(i);
+        gridContainer.appendChild(gridRow);
+
+        for (let j = 0; j < gridSize; j++) {
+            const squareDiv = createSquareDiv(); 
+            gridRow.appendChild(squareDiv);
+        }
     }
 }
 
@@ -24,11 +29,34 @@ function createSquareDiv() {
     return squareDiv;
 }
 
-gridContainer.addEventListener("mouseover", addBackgroundColor);
-
 function addBackgroundColor (event) {
     const squareDiv = event.target;
     if (!squareDiv.classList.contains("color-square")) { 
         squareDiv.classList.add("color-square");
     }
 }
+
+function changeGrid () {
+    const newGridSize = getNewGridSize();    
+    removeGrid();
+    createGrid(newGridSize);
+}
+
+function getNewGridSize () {
+    let newGridSize;
+    do {
+        newGridSize = parseInt(prompt("Enter new square grid size (> 0 & <=100):"));
+        console.log(newGridSize)
+    } while(newGridSize > 100 || newGridSize <= 0)
+
+    return newGridSize;
+}
+
+function removeGrid () {
+    const gridRowList = document.querySelectorAll("div.grid-row");
+    for (const gridRow of gridRowList) {
+        gridContainer.removeChild(gridRow);
+    }
+}
+
+createGrid(16);
